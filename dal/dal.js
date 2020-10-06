b5b56bf84a5c7ee5bfe8694103d16182b764e758
Yarden125 <yardenmor125@gmail.com>
2020-10-06 16:15:58 +0300

    changes

--- dal/dal.js
+++ dal/dal.js
@@ -6,7 +6,7 @@ const mysql = require("mysql");
 
 
 // Create a communication line to MySQL Database: 
-const connection = process.env.DATABASE_URL || mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
