30ca91d4156e565854e2ff9e21d3077b645cfe3b
Yarden125 <yardenmor125@gmail.com>
2020-10-07 11:28:30 +0300

    changes

--- client/src/services/api-service.ts
+++ client/src/services/api-service.ts
@@ -10,6 +10,7 @@ class ApiService{
 
     // Fetch - Post method
     private async preformFetchPost(url:string, bodyData:any){
+        console.log(url);
         const response = await fetch(url, {
             method: "POST",
             headers: {
